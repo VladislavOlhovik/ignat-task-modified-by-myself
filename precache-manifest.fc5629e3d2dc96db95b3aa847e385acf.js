71a8f556f58da576618da3a2796f4dcf9d96f7e1
Vladislav Olhovik <alkhovikvlad@gmail.com>
2020-09-24 15:04:06 +0200

    Updates

---
+++ precache-manifest.fc5629e3d2dc96db95b3aa847e385acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4beca7775b95bebb5677344f877a081",
+    "url": "/ignat-task-modified-by-myself/index.html"
+  },
+  {
+    "revision": "a284b3aa91ce2028900f",
+    "url": "/ignat-task-modified-by-myself/static/css/main.7d461902.chunk.css"
+  },
+  {
+    "revision": "ccae4b24abd21bef0473",
+    "url": "/ignat-task-modified-by-myself/static/js/2.424579ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ignat-task-modified-by-myself/static/js/2.424579ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a284b3aa91ce2028900f",
+    "url": "/ignat-task-modified-by-myself/static/js/main.8e35210a.chunk.js"
+  },
+  {
+    "revision": "aa8e81a12f140a888a9a",
+    "url": "/ignat-task-modified-by-myself/static/js/runtime-main.3f421f76.js"
+  }
+]);
